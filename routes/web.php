dc8a4a850b2a8b51408aa13508edb67c4446aae2
way <lzly6191@gmail.com>
2023-08-18 22:01:53 +0900

    调整uri

--- routes/web.php
+++ routes/web.php
@@ -10,26 +10,30 @@ use App\Http\Controllers\NoteController;
 Route::get('/', function () {
     return Inertia::render('Index');
 });
+
 // 笔记
 Route::prefix('/note')->group(function () {
     // 跳转笔记上传页面
-    Route::get('/create', function () {
-        return Inertia::render('Note/Create', [
-            'menu' => request()->menu,
-            'book' => request()->book
-        ]);
-    });
+    Route::get('/create', [NoteController::class, 'create'])
+        ->name('note.create');
     // 笔记上传保存
-    Route::post('/upload', [NoteController::class, 'upload']);
-    // 点击页头目录跳转
-    Route::get('/{menuName}', [MenuController::class, 'show']);
-    // 页头目录创建
-    Route::post('/menu/store', [MenuController::class, 'store']);
-    // 创建书
-    Route::post('/book/store', [BookController::class, 'store']);
-    // 点击书
-    Route::get('/{menuName}/{bookName}', [BookController::class, 'show']);
+    Route::post('/upload', [NoteController::class, 'upload'])
+        ->name('note.upload');
     // 点击笔记
     Route::get('/{menuName}/{bookName}/{noteName}', [NoteController::class, 'show'])
         ->name('note.show');
+
+    // 页头目录创建
+    Route::post('/menu/store', [MenuController::class, 'store'])
+        ->name('menu.store');
+    // 点击页头目录跳转
+    Route::get('/{menuName}', [MenuController::class, 'show'])
+        ->name('menu.show');
+
+    // 创建书
+    Route::post('/book/store', [BookController::class, 'store'])
+        ->name('book.store');
+    // 点击书
+    Route::get('/{menuName}/{bookName}', [BookController::class, 'show'])
+        ->name('book.show');
 });
