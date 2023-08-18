0998b9050f196737b8a2451478e0a7352ababe68
way <lzly6191@gmail.com>
2023-08-18 21:38:26 +0900

    add ziggy

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @vite('resources/css/app.css')
-    @vite('resources/js/app.js')
+    @routes
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
     @inertiaHead
 </head>
 
